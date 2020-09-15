7c266fa9e2510dff0c46b760bd7eaffb70507801
Kemal Ozturk <51798197+ozturkkl@users.noreply.github.com>
2020-09-15 15:17:48 -0400

    Added the index route

--- src/main/java/com/plantplaces/PlantPlacesController.java
+++ src/main/java/com/plantplaces/PlantPlacesController.java
@@ -3,10 +3,19 @@ package com.plantplaces;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+/**
+ * @author 97kem
+ *
+ */
 @Controller
 public class PlantPlacesController {
 	@RequestMapping("/start")
 	public String start() {
+		
+		return "start";
+	}
+	@RequestMapping("/")
+	public String index() {
 		
 		return "start";
 	}
