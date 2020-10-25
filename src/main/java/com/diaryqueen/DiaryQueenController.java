8e210f0fd68df930cc6ef38f42dc8dbd27f86b46
Administrator <Administrator@DESKTOP-099UQRG>
2020-10-24 23:04:05 -0400

    Deleted Excess code, Changed title in html page.

--- src/main/java/com/diaryqueen/DiaryQueenController.java
+++ src/main/java/com/diaryqueen/DiaryQueenController.java
@@ -17,8 +17,11 @@ public class DiaryQueenController {
 	
 	@RequestMapping(value="/", method=RequestMethod.GET)
 	public String index(Model model) {
+		
 		DiaryEntry diaryEntry1 = new DiaryEntry(1, "10/21/2020", "Wednesday Entry", "Today I was doing something and then I started doing something else... While I was doing that something else, I just realized that I was indeed doing nothing. This realization brought me to my feet just to yawn and stretch before going back to what I was doing earlier.");
 		DiaryEntry diaryEntry2 = new DiaryEntry(2, "10/22/2020", "What a phenomenal day!", "Today I was doing something and then I started doing something else... While I was doing that something else, I just realized that I was indeed doing nothing. This realization brought me to my feet just to yawn and stretch before going back to what I was doing earlier.");
+		
+		
 		
 		model.addAttribute("diaryEntry1", diaryEntry1);
 		model.addAttribute("diaryEntry2", diaryEntry2);
@@ -26,18 +29,5 @@ public class DiaryQueenController {
 		return "index";
 	}
 	
-//	@RequestMapping(value="/", method=RequestMethod.GET, params = {"loyalty=blue"})
-//	public String readBlue() {
-//		return "start";
-//	}
-//	@RequestMapping(value="/start", method=RequestMethod.GET, headers = {"content-type=text/json"})
-//	public String readJSON() {
-//		return "start";
-//	}
-	
-	
-//	@PostMapping("/start")
-//	public String create() {
-//		return "start";
-//	}
+
 }
