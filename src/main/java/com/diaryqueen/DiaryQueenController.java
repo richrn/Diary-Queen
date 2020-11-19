e973f58df4732e49c6b55a08f7c749a913ad959a
richrn <richrn@mail.uc.edu>
2020-11-18 19:06:50 -0500

    added reading of html elements of the add page to the controller
    added reading of html elements of the add page to the controller
    *Working*

--- src/main/java/com/diaryqueen/DiaryQueenController.java
+++ src/main/java/com/diaryqueen/DiaryQueenController.java
@@ -1,9 +1,20 @@
 package com.diaryqueen;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+ 
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import java.util.ArrayList;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -15,13 +26,34 @@ import com.diaryqueen.dto.DiaryEntry;
  */
 @Controller
 public class DiaryQueenController {
-
+	
+	
+	@WebServlet("/add")
+	public class ProcessText extends HttpServlet {
+	 
+	    protected void doPost(HttpServletRequest request,
+	            HttpServletResponse response) throws ServletException, IOException {
+	         
+	        // read form fields
+	    	String addTextBox = request.getParameter("addTextBox") ;
+	        String header = request.getParameter("header");
+	         
+	        System.out.println("addTextBox" + addTextBox);
+	        System.out.println("header: " + header);
+	        
+	        // Return Response 
+	    }
+	    }
+	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String index(Model model) {
 
 		ArrayList<DiaryEntry> entries = new ArrayList<>();
 		
-		entries.add(new DiaryEntry(1, java.time.LocalDate.now(), "Wednesday Entry", 
+		
+		
+		
+		entries.add(new DiaryEntry(1, java.time.LocalDate.now(),  "Wednesday Entry", 
 				"Today I was doing something and then I started doing something else... "
 				+ "While I was doing that something else, I just realized that I was indeed doing nothing. "
 				+ "This realization brought me to my feet just to yawn and stretch before going back to what I was doing earlier."));
@@ -31,14 +63,18 @@ public class DiaryQueenController {
 				+ "While I was doing that something else, I just realized that I was indeed doing nothing. "
 				+ "This realization brought me to my feet just to yawn and stretch before going back to what I was doing earlier."));
 		
+		
 
 		model.addAttribute("entries", entries);
 
 		return "index";
 	}
+
+	       
 	
 	@RequestMapping(value = "/add", method = RequestMethod.GET)
 	public String add(Model model) {
+		
 		return "add";
 	}
 	
